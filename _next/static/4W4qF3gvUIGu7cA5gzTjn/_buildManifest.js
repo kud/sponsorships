35bff539c1bdb9b9febdb7a328b80944ff77ceda
Erwann Mest <m@kud.io>
2021-02-01 11:04:15 +0100

    Updates

---
+++ _next/static/4W4qF3gvUIGu7cA5gzTjn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-00066c6150fe93565a98.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1bf508fb5c0e872cefaa.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/u0qEGjRLycErI6oyP3yxJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-2680fde4672d88bf0640.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1bf508fb5c0e872cefaa.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
